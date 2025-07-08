8b899514f6dab3826c4781bcb794e5599e38aaf9
Kwon <xogns12354@naver.com>
2025-07-08 15:54:00 +0900

    초기 커밋.

---
+++ Project1/first.cpp
@@ -0,0 +1,21 @@
+int main()
+{
+	int i = 0;
+
+	//자료형 (크기단위 byte=(8bit))
+	//정수형: char(1), short(2), int(4), long(4), longlong(8)
+	//실수형: float(4), double(8), long double(16)
+	//unsigned는 양의 정수만 표현한다. 
+	//MSB는 most siginificnat bit 라는 의미로 부호를 결정함. 1은 음수 0은 양수
+	//컴퓨터는 음수의 표현을 합이 0이 되는 수로 한다.
+	//2의 보수법을 이용하면 sighed에서 unsighed의 값으로 음수를 표현할 수 있다.
+
+	int i = 0;
+
+
+	unsigned char c = 0;
+	c = 0;
+	c = 255;
+
+	c = -1;
+}
